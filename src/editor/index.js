8b7e85cd3385f1baf8322b0d081a734eb9e12ca9
MetaEditor <107377084+meta-editor@users.noreply.github.com>
2023-01-10 16:04:51 +0800

    Update index.js

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,11 +1,21 @@
-import constructor from "./constructor";
+import Editor from "./editor";
 import "./index.less";
 
 const renderEditor = () => {
-  const editor = new constructor({
+  const editor = new Editor({
     editorElement: document.getElementById("root"),
   });
-  editor.init({});
+  editor.init({
+    toolbarItem: [
+      ["undo", "redo"],
+      ["h1", "h2", "h3", "h4", "h5", "h6"],
+      ["bold", "italic", "underline", "strikethrough", "quote", "code", "mark", "sup", "sub"],
+      [9, 10, 11, 12, 13, 14],
+      ["#b943ff", "#ff4343", "#4395ff"],
+      ["orderedlist", "unorderedlist", "tasklist"],
+      ["indent", "outdent", "alignment"],
+    ],
+  });
 };
 
 export default renderEditor;
