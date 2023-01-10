154e4da89540a0d82712049c9202d268cf5adcba
MetaEditor <107377084+meta-editor@users.noreply.github.com>
2023-01-10 15:31:39 +0800

    Update dragover.js

--- src/engine/helper/dragover.js
+++ src/engine/helper/dragover.js
@@ -2,7 +2,6 @@ import { getCardRoot } from "../utils/card";
 import { getDocument } from "../utils/node";
 /**
  * @fileOverview 编辑态
- * @author huangtonger@aliyun.com
  */
 const CARD_CURSOR_WIDTH = 2;
 
