be592806870a89ac4f323b5b1ae5c3264a133b50
MetaEditor <107377084+meta-editor@users.noreply.github.com>
2023-01-10 15:53:10 +0800

    Update constants.js

--- src/editor/constants.js
+++ src/editor/constants.js
@@ -3,9 +3,19 @@ export const TOOLBAR_BTN_CLS = "toolbar-btn";
 
 export const headingGroup = ["h1", "h2", "h3", "h4", "h5", "h6"];
 export const listGroup = ["orderedlist", "unorderedlist", "tasklist"];
+export const fontsizeGroup = [9, 10, 11, 12, 13, 14];
+export const fontcolorGroup = ["#b943ff", "#ff4343", "#4395ff"];
+// export const backcolorGroup = ["#b943ff", "#ff4343", "#4395ff"];
 
 export const defaultToolbarItem = [
+  ["undo", "redo"],
   [...headingGroup],
-  ["bold", "italic", "underline", "quote"],
+  ["bold", "italic", "underline", "strikethrough", "quote", "code", "mark", "sup", "sub"],
+  // ["fontsize", "fontcolor", "backcolor"],
+  [...fontsizeGroup],
+  [...fontcolorGroup],
+  // [...backcolorGroup],
   [...listGroup],
+  ["indent", "outdent", "alignment"],
+  // ["removeformat"],
 ];
